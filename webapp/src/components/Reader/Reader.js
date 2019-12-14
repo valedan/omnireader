304500eefdb620bad8d4b26321da60c6c85d855a
Dan <dan@danmolloy.dev>
2019-12-14 11:11:02 +0000

    scroll content wrapper so scrollbar is at side of page instead of inset

--- webapp/src/components/Reader/Reader.js
+++ webapp/src/components/Reader/Reader.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_CHAPTER } from "../../queries/chapter";
 import { ReaderContent } from "./ReaderContent";
+import { useInterval } from "../../hooks/useInterval";
+import { useMutation } from "@apollo/react-hooks";
+import { UPDATE_PROGRESS } from "../../queries/chapter";
+import ProgressBar from "../shared/ProgressBar";
+import { Divider, Paper } from "@material-ui/core";
+import { ChapterNavBar } from "./ChapterNavBar";
 
 export const Reader = () => {
   const { id } = useParams();
+  const [progress, setProgress] = useState(null);
 
   const { loading, error, data } = useQuery(GET_CHAPTER, { variables: { id } });
+  const [sendProgress] = useMutation(UPDATE_PROGRESS);
+
+  const updateProgress = progress => {
+    setProgress(progress);
+  };
+
+  useInterval(() => {
+    if (!data || progress === data.chapter.progress) return;
+    sendProgress({
+      variables: { chapterId: data.chapter.id, progress: progress }
+    });
+  }, 1000);
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
+  const chapter = data.chapter;
 
-  const content = `<h1>${data.chapter.title}</h1>` + data.chapter.content;
+  const content = `<h1>${chapter.title}</h1>` + chapter.content;
 
   return (
     <Wrapper>
-      {data && (
+      {chapter && (
         <Inner>
-          <ReaderContent chapter={data.chapter} content={content} />
+          <TopNav>{<ChapterNavBar chapter={chapter} />}</TopNav>
+          <Divider style={{ alignSelf: "stretch" }} />
+          <ReaderContent
+            updateProgress={updateProgress}
+            chapter={chapter}
+            content={content}
+          />
+          <ProgressWrapper>
+            {/* TODO: For some reason, can't style ProgressBar directly */}
+            {/* TODO: progress is momentarily stale when changing chapter */}
+            <ProgressBar value={progress * 100} showPercent />
+          </ProgressWrapper>
         </Inner>
       )}
     </Wrapper>
@@ -27,16 +59,35 @@ export const Reader = () => {
 
 const Wrapper = styled.div`
   margin: auto;
-  @media (min-width: 1200px) {
-  }
-  display: flex;
-  flex-direction: column;
 `;
 
-const Inner = styled.div`
+const Inner = styled(Paper)`
   display: flex;
   width: 100%;
-  flex-direction: row;
+  max-height: 100vh;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
+`;
+
+const TopAndBottom = `
+padding-left: 4vw;
+padding-right: 4vw;
+  @media (min-width: 900px) {
+    max-width: 900px;
+  }
+  @media (max-width: 900px) {
+    max-width: 900px;
+    padding-left: 4vw;
+    padding-right: 4vw;
+  }
+  width: 100%;
+`;
+const ProgressWrapper = styled.div`
+  ${TopAndBottom}
+  margin-top: 12px;
+`;
+
+const TopNav = styled.div`
+  ${TopAndBottom}
 `;
--- webapp/src/components/Reader/ReaderContent.js
+++ webapp/src/components/Reader/ReaderContent.js
@@ -1,34 +1,18 @@
 import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
-import { UPDATE_PROGRESS } from "../../queries/chapter";
-import { useMutation } from "@apollo/react-hooks";
-import { useInterval } from "../../hooks/useInterval";
-import Paper from "@material-ui/core/Paper";
-import ProgressBar from "../shared/ProgressBar";
-import { Divider } from "@material-ui/core";
-import { ChapterNavBar } from "./ChapterNavBar";
 
-export const ReaderContent = ({ chapter, content }) => {
+export const ReaderContent = ({ chapter, content, updateProgress }) => {
   const ref = useRef(null);
-  const [progress, setProgress] = useState(chapter.progress);
-  const [updateProgress] = useMutation(UPDATE_PROGRESS);
   const scrollableHeight = () =>
     ref.current.scrollHeight - ref.current.clientHeight;
 
   const scroll = e => {
     const newProgress = ref.current.scrollTop / scrollableHeight();
-    setProgress(Number.parseFloat(newProgress.toPrecision(6)));
+    updateProgress(Number.parseFloat(newProgress.toPrecision(6)));
   };
 
-  useInterval(() => {
-    if (progress === chapter.progress) return;
-    updateProgress({
-      variables: { chapterId: chapter.id, progress: progress }
-    });
-  }, 1000);
-
   useEffect(() => {
-    setProgress(chapter.progress);
+    updateProgress(chapter.progress);
     const current = ref.current;
     current.scrollTo(0, chapter.progress * scrollableHeight());
     current.onscroll = scroll;
@@ -36,35 +20,24 @@ export const ReaderContent = ({ chapter, content }) => {
   }, [chapter.id]);
 
   return (
-    // TODO: The page should scroll with a sticky footer, instead of just the content scrolling.
-    <Wrapper elevation={4}>
-      <TopNav>{<ChapterNavBar chapter={chapter} />}</TopNav>
-      <Divider style={{ alignSelf: "stretch" }} />
+    <Wrapper ref={ref}>
       {/* TODO: Security */}
-      <Content ref={ref} dangerouslySetInnerHTML={{ __html: content }} />
-      <ProgressWrapper>
-        {/* TODO: For some reason, can't style ProgressBar directly */}
-        <ProgressBar value={progress * 100} showPercent />
-      </ProgressWrapper>
+      <Content dangerouslySetInnerHTML={{ __html: content }} />
     </Wrapper>
   );
 };
 
-const Wrapper = styled(Paper)`
-  @media (max-width: 1200px) {
-    width: 100%;
-    max-height: 100vh;
-  }
+const Wrapper = styled.div`
+  width: 100%;
   @media (min-width: 1200px) {
     min-width: 900px;
-    width: 100%;
-
-    max-height: 100vh;
   }
   align-self: center;
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
+  overflow: auto;
 `;
 
 const Content = styled.div`
@@ -77,29 +50,6 @@ const Content = styled.div`
     max-width: 900px;
     padding-top: 0;
   }
-  overflow: auto;
   line-height: 1.7;
   font-size: 1.05em;
-  margin-bottom: 10px;
-`;
-
-const TopAndBottom = `
-padding-left: 4vw;
-padding-right: 4vw;
-  @media (min-width: 900px) {
-    max-width: 900px;
-  }
-  @media (max-width: 900px) {
-    max-width: 900px;
-    padding-left: 4vw;
-    padding-right: 4vw;
-  }
-  width: 100%;
-`;
-const ProgressWrapper = styled.div`
-  ${TopAndBottom}
-`;
-
-const TopNav = styled.div`
-  ${TopAndBottom}
 `;
