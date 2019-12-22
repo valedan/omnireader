dcbf1f2001f697f0ade4c9093a51d1150f6cfd76
Dan <dan@danmolloy.dev>
2019-12-22 14:48:57 +0000

    heroku deploy

---
+++ webapp/build/precache-manifest.6e51100719ef0d7c334196b502fa45fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f5f2590f52f5ee1632efbdfcfbb1aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "367441c2ed0d77ddc642",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2eaa5175df530ee2ebb6",
+    "url": "/static/css/main.e29d4fad.chunk.css"
+  },
+  {
+    "revision": "367441c2ed0d77ddc642",
+    "url": "/static/js/2.d41f7d12.chunk.js"
+  },
+  {
+    "revision": "2eaa5175df530ee2ebb6",
+    "url": "/static/js/main.b63c0ce8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
